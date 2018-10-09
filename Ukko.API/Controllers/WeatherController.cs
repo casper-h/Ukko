7439e98cd38de04c669a9df5673a06ad9b77fc12
Chris Lamb <christopher.lamb@protonmail.com>
2018-10-09 17:34:54 -0400

    Adding swagger docs #14

--- Ukko.API/Controllers/WeatherController.cs
+++ Ukko.API/Controllers/WeatherController.cs
@@ -20,6 +20,56 @@ namespace Ukko.API.Controllers
             this.owms = new OpenWeatherMapService();
         }
 
+        /// <summary>
+        /// Gets the current weather for a given zipcode.
+        /// </summary>
+        /// <param name="zipCode">The zipcode for a users area</param>
+        /// <returns>
+        /// {
+        ///    "coord": {
+        ///        "lon": -82.79,
+        ///        "lat": 28.03
+        ///    },
+        ///    "sys": {
+        ///        "type": 1,
+        ///        "id": 726,
+        ///        "message": 0.005,
+        ///        "country": "US",
+        ///        "sunrise": 1539084521,
+        ///        "sunset": 1539126431
+        ///    },
+        ///    "weather": [
+        ///        {
+        ///            "id": 803,
+        ///            "main": "Clouds",
+        ///            "description": "broken clouds",
+        ///            "icon": "04d"
+        ///        }
+        ///    ],
+        ///    "base": "stations",
+        ///    "main": {
+        ///        "temp": 304.87,
+        ///        "humidity": 59,
+        ///        "pressure": 1008,
+        ///        "temp_min": 304.25,
+        ///        "temp_max": 305.35
+        ///    },
+        ///    "wind": {
+        ///        "speed": 5.1,
+        ///        "deg": 100
+        ///    },
+        ///    "clouds": {
+        ///        "all": 75
+        ///    },
+        ///    "dt": 1539117300,
+        ///    "id": 420009842,
+        ///    "name": "Clearwater",
+        ///    "cod": 200
+        /// }
+        /// </returns>
+        /// <example>
+        /// /api/Weather/12345
+        /// </example>
         [HttpGet("{zipCode}")]
         [ProducesResponseType(typeof(CurrentWeather), 200)]
         public async Task<IActionResult> GetCurrentWeatherByZipCodeAsync(uint zipCode)
--- Ukko.API/Startup.cs
+++ Ukko.API/Startup.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -10,17 +12,20 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Swashbuckle.AspNetCore.Swagger;
 
 namespace Ukko.API
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+        public IHostingEnvironment Environment { get; }
+        public IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration configuration, IHostingEnvironment env)
         {
             Configuration = configuration;
+            Environment = env;
         }
-
-        public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
@@ -34,7 +39,21 @@ namespace Ukko.API
 
                 cors.DefaultPolicyName = "AllowAll";
             });
-            
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info
+                {
+                    Version = "v1",
+                    Title = "Ukko API",
+                });
+
+                var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.XML";
+                var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+
+                c.IncludeXmlComments(xmlPath);
+            });
+
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
 
@@ -49,6 +68,13 @@ namespace Ukko.API
             {
                 app.UseHsts();
             }
+
+            app.UseSwagger();
+
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Ukko API");
+            });
 
             app.UseHttpsRedirection();
             app.UseMvc();
